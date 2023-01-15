134f743d9e32ae7b2448a9858a1409372063b1dd
S-Doody <seanjd90@gmail.com>
2023-01-15 15:10:26 -0500

    Add decade and genre arrays, and song quote object

--- main.js
+++ main.js
@@ -4,6 +4,31 @@ const randElement = arr => {
   return element;
 };
 
+//Decade Array
+const decadeArr = ['80s', '90s', '2000s'];
+ 
+//Genre Array
+const genreArr = ['Rock', 'Pop', 'Hip-hop'];
+ 
+//Song Quote Object
+const songQuotes = {
+ Eighties: {
+   Rock: ['er0', 'er1', 'er2'],
+   Pop: ['ep0', 'ep1', 'ep2'],
+   hipHop: ['eh0', 'eh1', 'eh2'],
+ },
+ Nineties: {
+   Rock: ['er0', 'er1', 'er2'],
+   Pop: ['np0', 'np1', 'np2'],
+   hipHop: ['nh0', 'nh1', 'nh2'],
+ },
+ twoThousands: {
+   Rock: ['tr0', 'tr1', 'tr2'],
+   Pop: ['tp0', 'tp1', 'tp2'],
+   hipHop: ['th0', 'th1', 'th2'],
+ }
+ 
+};
 
 
 
